7f6c1b55d91f6b04caf43a89ff9e7baf44f4e148
k1ut3h <hardikedu811@gmail.com>
2023-04-30 12:26:46 +0530

    app made secure

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,13 +1,14 @@
 import { Client } from "discord.js";
 import ready from "./listeners/ready"
+import token from "./config.json";
 
-const token = "MTAwOTQ1ODk1NTA2OTY4OTk2Ng.G-ZjbA.ilWSbq0BAVaXNJbi7UH2T2cp_Uz2DEuPfPrMoQ";
 console.log("Bot is starting...");
 
 const client = new Client({
 	intents: []
 });
+
 ready(client);
-client.login(token);
+client.login(token.token);
 
 console.log(client);
